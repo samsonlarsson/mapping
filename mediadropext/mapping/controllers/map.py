a9a2b0fc836dc702e1936873b7a70fc8a63d9b81
Samson Paul Jnr <hello@samsonpaul.me>
2017-11-06 09:41:23 +0100

    updates map controller

--- mediadropext/mapping/controllers/map.py
+++ mediadropext/mapping/controllers/map.py
@@ -3,7 +3,18 @@
 from mediadrop.lib.base import BaseController
 from mediadrop.lib.decorators import expose
 
-class SampleController(BaseController):
+import googlemaps
+
+gmaps = googlemaps.Client(key='AIzaSyA3mWehI-WUOmQlTs7h5sV339EcL484DHQ')
+
+# Geo-coding an address
+geocode_result = gmaps.geocode('Siera Leone & Liberia')
+
+# Look up an address with reverse geocoding
+reverse_geocode_result = gmaps.reverse_geocode((7.493196470122287, -10.1953125))
+
+
+class MapController(BaseController):
     @expose('mapping/map.html')
     def index(self, value=None, **kwargs):
         # do your backend work here
